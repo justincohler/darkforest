4c06010e6857b36fc5f0964868a3ce32acd473b5
Justin Cohler <justincohler@gmail.com>
2017-11-12 18:42:13 -0600

    init commit

---
+++ point.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+class Point:
+
+    def __init__(self, buy, sell):
+        self.buy_price=buy["amount"]
+        self.sell_price=sell["amount"]
+        self.base=buy["base"]
+        self.currency=buy["currency"]
+        self.dt = datetime.now()
+
+    def __repr__(self):
+        return repr([self.buy_price, self.sell_price, self.base, self.currency, self.dt])
