e88c83601b51e2baf5a9216829f010f8b5942e8d
Justin Cohler <justincohler@gmail.com>
2017-11-17 15:27:23 -0600

    Add pydoc comments

--- ochl.py
+++ ochl.py
@@ -1,8 +1,16 @@
+"""
+ochl is a container for the OCHL class.
+
+Author: Justin Cohler
+Created: 11/17/2017
+"""
 from datetime import datetime
 
 class OCHL:
+    """OCHL contains point-in-time data for an OCHL (open, high, low, close)."""
 
     def __init__(self, o, c, h, l, bid, ask, volume, vwap, date=None):
+        """Construct using several data from Quandl API."""
         self.date = date if date else datetime.now().date
         self.open = o
         self.close = c
@@ -14,9 +22,11 @@ class OCHL:
         self.vwap = vwap
 
     def formatted(self):
+        """Return a formatted version, compliant with Matplotlib."""
         return [self.date.toordinal(), self.open, self.close, self.high, self.low]
 
     def to_dict(self):
+        """Transform object to simple dict."""
         return {
             'date': self.date,
             'open': self.open,
--- point.py
+++ point.py
@@ -1,8 +1,20 @@
+"""
+point is a container for the Point class.
+
+Author: Justin Cohler
+Created: 11/17/2017
+"""
+
 from datetime import datetime
 
 class Point:
+    """Point contains point-in-time data for a given base and currency.
+
+    e.g. base: BTC, currency: USD
+    """
 
     def __init__(self, buy, sell):
+        """Construct using a buy and sell object from coinbase API."""
         self.buy_price=float(buy["amount"])
         self.sell_price=float(sell["amount"])
         self.base=buy["base"]
@@ -10,9 +22,11 @@ class Point:
         self.dt = datetime.now()
 
     def __repr__(self):
+        """Represent as a list of object attributes."""
         return repr([self.buy_price, self.sell_price, self.base, self.currency, self.dt])
 
     def to_dict(self):
+        """Transform object to simple dict."""
         return {
             'buy_price': self.buy_price,
             'sell_price': self.sell_price,
